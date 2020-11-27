41f768eda3a8480cbd793eb5db6ce6ca9e7e9df2
Opilane TTHK <opilane@tthk.ee>
2020-11-27 10:06:16 +0200

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        class Cat
+        {
+            string name;
+            string color;
+            double hunger;
+
+            public Cat(string _name, string _color)
+            {
+                name = _name;
+                color = _color;
+                hunger = 0;
+            }
+            public string Name
+            {
+                get { return name; }
+                set
+                {
+                    Name = value;
+                }
+            }
+            public string Color
+            {
+                get { return color; }
+            }
+            public double Hunger
+            {
+                get { return hunger; }
+            }
+            public void Sleep()
+            {
+                Console.WriteLine("The cat sleeps");
+                hunger += 0.2;
+                Console.WriteLine($"cat is hungry:{hunger}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Cat newcat = new Cat("cat", "brown");
+            while (newcat.Hunger != 1)
+            {
+                newcat.Sleep();
+            }
+
+            Console.WriteLine("meow");
+        }
+    }
+}
+
